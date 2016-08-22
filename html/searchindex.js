2a1ef1a92586669401d5f274af31287c5cc96d37
Ana Ruelas <aruelas@quantopian.com>
2016-08-22 14:54:37 -0400

    DOC: Rebuilt docs

---
+++ html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:50,filenames:["includeme","index"],objects:{},objnames:{},objtypes:{},terms:{content:1,index:1,modul:1,page:1,search:1},titles:["&lt;no title&gt;","Welcome to empyrical&#8217;s documentation!"],titleterms:{document:1,empyr:1,indice:1,tabl:1,welcom:1}})
